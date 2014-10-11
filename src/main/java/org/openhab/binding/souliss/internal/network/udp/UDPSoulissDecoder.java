3216f61cb4e45dd50f9aa40ed6c9b45027fccb5b
fazioa <fazioa@gmail.com>
2014-10-12 00:01:23 +0200

    -

--- src/main/java/org/openhab/binding/souliss/internal/network/udp/UDPSoulissDecoder.java
+++ src/main/java/org/openhab/binding/souliss/internal/network/udp/UDPSoulissDecoder.java
@@ -1,28 +1,13 @@
 package org.openhab.binding.souliss.internal.network.udp;
 
-import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import org.openhab.binding.souliss.internal.network.typicals.Constants;
 import org.openhab.binding.souliss.internal.network.typicals.SoulissGenericTypical;
 import org.openhab.binding.souliss.internal.network.typicals.SoulissNetworkParameter;
-import org.openhab.binding.souliss.internal.network.typicals.SoulissT11;
-import org.openhab.binding.souliss.internal.network.typicals.SoulissT22;
-import org.openhab.binding.souliss.internal.network.typicals.SoulissT57;
 import org.openhab.binding.souliss.internal.network.typicals.SoulissTServiceUpdater;
 import org.openhab.binding.souliss.internal.network.typicals.SoulissTypicals;
 
@@ -52,8 +37,6 @@ public class UDPSoulissDecoder {
 	 */
 	public void decodeVNetDatagram(DatagramPacket packet) {
 		int checklen = packet.getLength();
-
-		// Log.d(Constants.TAG, "** Packet received");
 		ArrayList<Short> mac = new ArrayList<Short>();
 		for (int ig = 7; ig < checklen; ig++) {
 			mac.add((short) (packet.getData()[ig] & 0xFF));
@@ -68,7 +51,6 @@ public class UDPSoulissDecoder {
 	 */
 	private void decodeMacaco(ArrayList<Short> macacoPck) {
 		int functionalCode = macacoPck.get(0);
-		//System.out.println("received functional code: 0x" + Integer.toHexString(functionalCode) );
 		LOGGER.info("Received functional code: 0x" + Integer.toHexString(functionalCode) );
 		switch (functionalCode) {
 		
@@ -94,130 +76,27 @@ public class UDPSoulissDecoder {
 		//			decodeTypRequest(macacoPck);
 		//			break;
 		case (byte) ConstantsUDP.Souliss_UDP_function_health_resp:// Answer nodes healty
-			//	Log.d(Constants.TAG, "** Health answer");
 			LOGGER.info("function_health_resp");
 			decodeHealthRequest(macacoPck);
 			break;
 		case (byte) ConstantsUDP.Souliss_UDP_function_db_struct_resp:// Answer nodes
-			//assertEquals(macacoPck.size(), 9); // healty
-			//	Log.w(Constants.TAG, "** DB Structure answer");
 			LOGGER.info("function_db_struct_resp");
 			decodeDBStructRequest(macacoPck);
 		break;
 		case 0x83:
-			//Log.e(Constants.TAG, "** (Functional code not supported)");
 			LOGGER.info("Functional code not supported");
 			break;
 		case 0x84:
-			//Log.e(Constants.TAG, "** (Data out of range)");
 			LOGGER.info("Data out of range");
 			break;
 		case 0x85:
-			//Log.e(Constants.TAG, "** (Subscription refused)");
 			LOGGER.info("Subscription refused");
 			break;
 		default:
 			LOGGER.info("Unknown functional code");
-			//	Log.e(Constants.TAG, "** Unknown functional code: " + functionalCode);
 			break;
 		}
-
 	}
-
-	//	private void processTriggers() {
-	//		try {
-	//
-	//			List<SoulissNode> ref = database.getAllNodes();
-	//			List<SoulissTrigger> triggers = database.getAllTriggers(context);
-	//			Log.i(Constants.TAG, "checked triggers: " + triggers.size());
-	//			// logThings(refreshedNodes);
-	//
-	//			Map<Short, SoulissNode> refreshedNodes = new HashMap<Short, SoulissNode>();
-	//
-	//			/* Check antifurto */
-	//
-	//			for (SoulissNode soulissNode : ref) {
-	//				refreshedNodes.put(soulissNode.getId(), soulissNode);
-	//				if (opzioni.isAntitheftPresent() && opzioni.isAntitheftNotify()) {// giro
-	//																					// i
-	//																					// tipici
-	//																					// solo
-	//																					// se
-	//																					// seve
-	//					for (SoulissTypical ty : soulissNode.getTypicals()) {
-	//						// check Antitheft
-	//						if (ty.getTypicalDTO().getTypical() == Souliss_T41_Antitheft_Main
-	//								&& ty.getTypicalDTO().getOutput() == Souliss_T4n_InAlarm) {
-	//							sendAntiTheftNotification(context, context.getString(R.string.antitheft_notify),
-	//									context.getString(R.string.antitheft_notify_desc), R.drawable.shield);
-	//							break;
-	//						}
-	//					}
-	//				}
-	//			}
-	//			for (SoulissTrigger soulissTrigger : triggers) {
-	//				SoulissCommandDTO command = soulissTrigger.getCommandDto();
-	//				SoulissTriggerDTO src = soulissTrigger.getTriggerDto();
-	//				SoulissTypical source = refreshedNodes.get(src.getInputNodeId()).getTypical(src.getInputSlotlot());
-	//				// SoulissTypical target =
-	//				// refreshedNodes.get(command.getNodeId()).getTypical(command.getSlot());
-	//				Calendar now = Calendar.getInstance();
-	//				if (!soulissTrigger.getTriggerDto().isActivated()) {
-	//					String op = src.getOp();
-	//					if (">".compareTo(op) == 0 && source.getTypicalDTO().getOutput() > src.getThreshVal()) {
-	//						Log.w(Constants.TAG, "TRIGGERING COMMAND " + command.toString());
-	//						soulissTrigger.getTriggerDto().setActive(true);
-	//						UDPHelper.issueSoulissCommand("" + command.getNodeId(), "" + command.getSlot(),
-	//								SoulissClient.getOpzioni(), command.getType(), "" + command.getCommand());
-	//						command.setExecutedTime(now);
-	//						soulissTrigger.persist(database);
-	//						SoulissDataService.sendNotification(context, command.toString(), soulissTrigger.toString(),
-	//								R.drawable.lighthouse);
-	//					} else if ("<".compareTo(op) == 0 && source.getTypicalDTO().getOutput() < src.getThreshVal()) {
-	//						Log.w(Constants.TAG, "TRIGGERING COMMAND " + command.toString());
-	//						soulissTrigger.getTriggerDto().setActive(true);
-	//						UDPHelper.issueSoulissCommand("" + command.getNodeId(), "" + command.getSlot(),
-	//								SoulissClient.getOpzioni(), command.getType(), "" + command.getCommand());
-	//						soulissTrigger.getCommandDto().setExecutedTime(now);
-	//						soulissTrigger.persist(database);
-	//						SoulissDataService.sendNotification(context, command.toString(), soulissTrigger.toString(),
-	//								R.drawable.lighthouse);
-	//					} else if ("=".compareTo(op) == 0 && source.getTypicalDTO().getOutput() == src.getThreshVal()) {
-	//						Log.w(Constants.TAG, "TRIGGERING COMMAND " + command.toString());
-	//						UDPHelper.issueSoulissCommand("" + command.getNodeId(), "" + command.getSlot(),
-	//								SoulissClient.getOpzioni(), command.getType(), "" + command.getCommand());
-	//						soulissTrigger.getTriggerDto().setActive(true);
-	//						soulissTrigger.getCommandDto().setExecutedTime(now);
-	//						soulissTrigger.persist(database);
-	//						SoulissDataService.sendNotification(context, command.toString(), soulissTrigger.toString(),
-	//								R.drawable.lighthouse);
-	//					}
-	//				}
-	//				// vedi se bisogna disattivare
-	//				else {
-	//					String op = src.getOp();
-	//					if (">".compareTo(op) == 0 && source.getTypicalDTO().getOutput() <= src.getThreshVal()) {
-	//						Log.w(Constants.TAG, "DEACTIVATE TRIGGER " + command.toString());
-	//						soulissTrigger.getTriggerDto().setActive(false);
-	//						soulissTrigger.persist(database);
-	//					} else if ("<".compareTo(op) == 0 && source.getTypicalDTO().getOutput() >= src.getThreshVal()) {
-	//						Log.w(Constants.TAG, "DEACTIVATE TRIGGER " + command.toString());
-	//						soulissTrigger.getTriggerDto().setActive(false);
-	//						soulissTrigger.persist(database);
-	//					} else if ("=".compareTo(op) == 0 && source.getTypicalDTO().getOutput() != src.getThreshVal()) {
-	//						Log.w(Constants.TAG, "DEACTIVATE TRIGGER " + command.toString());
-	//						soulissTrigger.getTriggerDto().setActive(false);
-	//						soulissTrigger.persist(database);
-	//					}
-	//				}
-	//
-	//			}
-	//		} catch (IllegalStateException e) {
-	//			Log.e(Constants.TAG, "DB connection was closed, check trigger impossible");
-	//			return;
-	//		}
-	//
-	//	}
 
 	//	/**
 	//	 * Alla ricezione di una risposta ping, aggiorna il cached address F e`
@@ -376,25 +255,12 @@ public class UDPSoulissDecoder {
 	}
 	
 	private float getFloatAtSlot(ArrayList<Short> mac, int slot) {
-			// refresh typicals
-			//for (short j = 0; j < numberOf; j=(short) (j+2)) {
-
 				int iOutput=mac.get(5 + slot);
-				//int iSlot = (short) (j % typXnodo);
-				//int iNodeId = (short) (j / typXnodo + tgtnode);
-
-//				System.out.println("-iNodeId: "+ iNodeId);
-//				System.out.println("-iSlot: "+ iSlot);
-//				//System.out.println("iOutput: "+ iOutput);
-
 				int iOutput2=mac.get(5 + slot+1);
 				//ora ho i due bytes, li converto
 				int shifted = iOutput2 << 8;
 				float ret = HalfFloatUtils.toFloat(shifted + iOutput);
-			//}
-		//}
-
-		return ret;
+				return ret;
 	}
 
 /**
@@ -405,10 +271,10 @@ public class UDPSoulissDecoder {
 	 */
 	private void decodeHealthRequest(ArrayList<Short> mac) {
 		// Threee static bytes
-		int tgtnode = mac.get(3);
+	//	int tgtnode = mac.get(3);
 		int numberOf = mac.get(4);
 
-		ArrayList<Short> healths = new ArrayList<Short>();
+	//	ArrayList<Short> healths = new ArrayList<Short>();
 		// build an array containing healths
 		for (int i = 5; i < 5 + numberOf; i++) {
 			//healths.add(Short.valueOf(mac.get(i)));
