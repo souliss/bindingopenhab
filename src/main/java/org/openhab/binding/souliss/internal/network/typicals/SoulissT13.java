86dd380980d00bc0b9a34d91b7533b23456975fd
fazioa <fazioa@gmail.com>
2014-10-14 00:31:28 +0200

    Supporto T12 e T13 (T12 da provare)

---
+++ src/main/java/org/openhab/binding/souliss/internal/network/typicals/SoulissT13.java
@@ -0,0 +1,36 @@
+package org.openhab.binding.souliss.internal.network.typicals;
+
+import java.util.ArrayList;
+
+import org.openhab.binding.souliss.internal.network.udp.SoulissCommGate;
+import org.openhab.core.library.types.OnOffType;
+import org.openhab.core.library.types.OpenClosedType;
+import org.openhab.core.types.State;
+
+public class SoulissT13 extends SoulissGenericTypical {
+//i parametri sSoulissNode, iSlot, Type, State vengono memorizzati nell'istanza della classe che estendo
+	
+	
+	public SoulissT13(String sSoulissNodeIPAddress, String sSoulissNodeIPAddressOnLAN, int iIDNodo, int iSlot, String sOHType) {
+		super();
+		this.setSlot(iSlot);
+		this.setSoulissNodeIPAddress(sSoulissNodeIPAddress);
+		this.setSoulissNodeIPAddressOnLAN(sSoulissNodeIPAddressOnLAN);
+	//	this.setSoulissNodeVNetAddress(sSoulissNodeVNetAddress);
+		this.setSoulissNodeID(iIDNodo);
+		this.setType(Constants.Souliss_T13);
+		this.setNote(sOHType);
+	}
+		
+	
+	//i parametri sSoulissNode, iSlot, Type, State vengono memorizzati nell'istanza della classe che estendo
+	
+	@Override
+	public State getOHState() {
+		String sOHState=StateTraslator.statesSoulissToOH(this.getNote(), this.getType(),this.getState());
+		if (this.getNote().equals("ContactItem"))
+			return OpenClosedType.valueOf(sOHState);
+		else return OnOffType.valueOf(sOHState);
+	}
+
+}
