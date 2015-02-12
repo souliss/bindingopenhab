031c48d8331b452658ee036413c4a06c77a5c825
fazioa <fazioa@gmail.com>
2015-02-12 02:25:13 +0100

    RGB Bugfix

--- openhab/bundles/binding/org.openhab.binding.souliss/src/main/java/org/openhab/binding/souliss/internal/SoulissBinding.java
+++ openhab/bundles/binding/org.openhab.binding.souliss/src/main/java/org/openhab/binding/souliss/internal/SoulissBinding.java
@@ -139,11 +139,6 @@ public class SoulissBinding<E> extends
 					SoulissNetworkParameter.NodeIndex = Integer
 							.parseInt((String) config.get(sName));
 					break;
-					//Parameter setted by SoulissTypicalsRecipients.getNodeNumbers();
-//				case "NODE_NUMBERS":
-//					SoulissNetworkParameter.nodes = Integer
-//							.parseInt((String) config.get(sName));
-//					break;
 				case "SERVERPORT":
 					if (config.get(sName).equals(""))
 						SoulissNetworkParameter.serverPort = null;
@@ -252,11 +247,7 @@ public class SoulissBinding<E> extends
 	 * @return short RGBList[] contain RGB components
 	 */
 	private short[] HSBtoRGB(Float H, Float S, Float B) {
-
-		int RGB = Color.HSBtoRGB(H, S, B);
-		Color c = new Color(RGB);
-		short RGBList[] = { (short) c.getRed(), (short) c.getGreen(),
-				(short) c.getBlue() };
+		short RGBList[] = hsvToRgb(H, S, B);
 		return RGBList;
 	}
 
@@ -327,5 +318,67 @@ public class SoulissBinding<E> extends
 	 */
 	public void putOnOHEventBus(String itemName, String value) {
 		eventPublisher.postUpdate(itemName, new StringType(value));
+	}
+	
+	
+	
+	public short[] hsvToRgb(float H, float S, float V) {
+		   float R, G, B;
+
+	        H /= 360f;
+	        S /= 100f;
+	        V /= 100f;
+
+	        if (S == 0)
+	        {
+	            R = V * 255;
+	            G = V * 255;
+	            B = V * 255;
+	        } else {
+	            float var_h = H * 6;
+	            if (var_h == 6)
+	                var_h = 0; // H must be < 1
+	            int var_i = (int) Math.floor((double) var_h); // Or ... var_i =
+	                                                            // floor( var_h )
+	            float var_1 = V * (1 - S);
+	            float var_2 = V * (1 - S * (var_h - var_i));
+	            float var_3 = V * (1 - S * (1 - (var_h - var_i)));
+
+	            float var_r;
+	            float var_g;
+	            float var_b;
+	            if (var_i == 0) {
+	                var_r = V;
+	                var_g = var_3;
+	                var_b = var_1;
+	            } else if (var_i == 1) {
+	                var_r = var_2;
+	                var_g = V;
+	                var_b = var_1;
+	            } else if (var_i == 2) {
+	                var_r = var_1;
+	                var_g = V;
+	                var_b = var_3;
+	            } else if (var_i == 3) {
+	                var_r = var_1;
+	                var_g = var_2;
+	                var_b = V;
+	            } else if (var_i == 4) {
+	                var_r = var_3;
+	                var_g = var_1;
+	                var_b = V;
+	            } else {
+	                var_r = V;
+	                var_g = var_1;
+	                var_b = var_2;
+	            }
+
+	            R = var_r * 255; // RGB results from 0 to 255
+	            G = var_g * 255;
+	            B = var_b * 255;
+	        }
+	    
+	    short RGBList[] = {(short) R, (short) G, (short) B };
+	    return RGBList;
 	}
 }
